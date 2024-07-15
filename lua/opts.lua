d442162a76bb3e4caedeba56dc45962e19b2a699
Fennel <fennel@cristata.local>
2024-07-15 01:53:49 -0400

    added quickfix keybind for LSP suggested edits

--- lua/opts.lua
+++ lua/opts.lua
@@ -13,12 +13,23 @@ vim.opt.sta = true
 vim.opt.ts = 4
 vim.opt.clipboard = "unnamedplus"
 
+local opts = { noremap = true, silent = true }
+
 vim.keymap.set('n', ']d', function()
 	vim.diagnostic.goto_next()
 	vim.diagnostic.open_float()
-end)
+end, opts)
 
 vim.keymap.set('n', '[d', function()
 	vim.diagnostic.goto_prev()
 	vim.diagnostic.open_float()
-end)
+end, opts)
+
+local function quickfix()
+	vim.lsp.buf.code_action({
+		filter = function(a) return a.isPreferred end,
+		apply = true
+	})
+end
+
+vim.keymap.set('n', '<leader>qf', quickfix, opts)
