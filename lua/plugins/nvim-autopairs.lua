1ff40f9ccf2b61f9971dd3892d8534edd5d02cf4
Fennel <fennel@everwild.dev>
2024-09-17 23:14:42 -0400

    added $ to autopairs for latex, typst

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -1,5 +1,12 @@
 return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
-	opts = {} -- this is equalent to setup({}) function
+	opts = {},
+	config = function(_, opts)
+		local npairs = require('nvim-autopairs')
+		local Rule = require('nvim-autopairs.rule')
+		npairs.setup(opts)
+		npairs.add_rule(Rule("$", "$", { "tex", "typst" }))
+		npairs.add_rule(Rule("$ ", " $", "typst"))
+	end
 }
